8e5def6725c84af8cf705959e1917376f8ff1911
Sai Zaw Myint <96133665+SaiZawMyint@users.noreply.github.com>
2025-05-01 19:45:26 +0630

    initial commit

---
+++ src/test/java/io/github/flexitech_realtime_provider/service_registry/ServiceRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.flexitech_realtime_provider.service_registry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
